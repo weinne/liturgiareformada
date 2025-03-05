4b714c0179bc4d47749d2e52388e2c26885a9945
Weinne Santos <weinnewillan@gmail.com>
2025-03-05 11:54:34 -0300

    Remove dependência do dotenv na configuração do Firebase

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,5 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
