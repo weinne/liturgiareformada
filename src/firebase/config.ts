6d675194655d40891250e14bb7f114fa35d33a10
Weinne Santos <weinnewillan@gmail.com>
2025-03-05 14:03:50 -0300

    Adiciona suporte ao App Check do Firebase com reCAPTCHA V3

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBRiLUDA_EuLKb4FZrLM8al1y4yx6s4nX4",
@@ -11,7 +12,16 @@ const firebaseConfig = {
   measurementId: "G-TDV1W1ZE85"
 };
 
+// Inicializa o Firebase
 const app = initializeApp(firebaseConfig);
+
+// Inicializa o App Check com reCAPTCHA V3 (substitua <SUA_SITE_KEY> pela chave obtida no Firebase Console)
+const appCheck = initializeAppCheck(app, {
+  provider: new ReCaptchaV3Provider("6LecneoqAAAAAIsq8mHOLiGuawzJYyuhdO4t3aaQ"),
+  // Habilita a renovação automática dos tokens
+  isTokenAutoRefreshEnabled: true
+});
+
 const db = getFirestore(app);
 
 export { db };
