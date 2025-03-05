31af45309e03763e6cc3a20360011244a4de5091
weinne <weinnewillan@gmail.com>
2025-03-05 08:00:40 -0300

    Corrigir formato dos links

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { LiturgyProvider } from "@/context/LiturgyContext";
 import { ThemeProvider } from "@/context/ThemeContext";
 import Index from "./pages/Index";
@@ -20,7 +20,7 @@ const App = () => (
         <LiturgyProvider>
           <Toaster />
           <Sonner />
-          <BrowserRouter>
+          <HashRouter>
             <Routes>
               <Route path="/" element={<Index />} />
               <Route path="/edit" element={<LiturgyEditor />} />
@@ -28,7 +28,7 @@ const App = () => (
               {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
               <Route path="*" element={<NotFound />} />
             </Routes>
-          </BrowserRouter>
+          </HashRouter>
         </LiturgyProvider>
       </ThemeProvider>
     </TooltipProvider>
